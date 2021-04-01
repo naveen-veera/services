a911df79f8b5d27e7305276256e1ba2ce4c74f68
Naveen132895 <veeranaveen.sethuraman@faceacademy.in>
2021-04-02 02:01:14 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,14 @@ export class AppComponent {
   secondDate:Date;
   firstString:String;
 
-  dc1 : any;
-  md1 : any;
-  cd1 : any;
-  fd1 : any;
-  ar1 : any;
-  ann1 : any;
-  as1 : any;
-  ve1 : any;
+  dc1 : string;
+  md1 : string;
+  cd1 : string;
+  fd1 : string;
+  ar1 : string;
+  ann1 : string;
+  as1 : string;
+  ve1 : string;
 
   constructor(private dateServices:DateService, private arrayService:ArrayService) {
 
@@ -31,11 +31,6 @@ export class AppComponent {
     var md = this.dateServices.monthDiff(postData);
     var cd = this.dateServices.compareDate(postData);
     var fd = this.dateServices.formatDate(postData);
-    
-    console.log("The Day difference is " + dc);
-    console.log("The Month difference is " + md);
-    console.log("The Dates comparison is " + cd);
-    console.log("The Formated Date is " + fd);
 
     this.dc1 = "The Day difference is " + dc;
     this.md1 = "The Month difference is " + md;
@@ -49,17 +44,10 @@ export class AppComponent {
   onArrSubmit(postData) {
   
     var ar = this.arrayService.arrayReverse(postData);
-    console.log("The Reversed Array is " + ar);
-
     var ann = this.arrayService.arrayNull(postData);
-    console.log("Array Null or Empty Check is " + ann);
-
     var as = this.arrayService.arraySort(postData);
-    console.log("The Sorted Array is " + as);
-    
     var ve = this.arrayService.searchArray(postData);
-    console.log("The Value Status is " + ve);
-
+    
     this.ar1 = "The Reversed Array is " + ar;
     this.ann1 = "Array Null or Empty Check is " + ann;
     this.as1 = "The Sorted Array is " + as;
--- src/app/date.service.spec.ts
+++ src/app/date.service.spec.ts
@@ -10,7 +10,22 @@ describe('DateService', () => {
     service = TestBed.inject(DateService);
   });
 
-  it('should be created', () => {
+  it('test_case1', () => {
     expect(service).toBeTruthy();
   });
+
+  it('test_case2', () => {
+    let result = service.compareDate({firstDate: '07-03-1998',secondDate: '07-03-2021'})
+    expect(result).toBe('Date 2 > Date 1');
+  });
+
+  it('test_case3', () => {
+    let result = service.monthDiff({firstDate: '07-03-1998',secondDate: '07-03-2021'})
+    expect(result).toBe('276');
+  });
+
+  it('test_case4', () => {
+    let result = service.daysCount({firstDate: '07-03-1998',secondDate: '07-03-2021'})
+    expect(result).toBe(8401);
+  }); 
 });
